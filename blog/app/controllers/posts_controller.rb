f5b22489d92236e587778f8af829f2ee4c34bd10
anilbhu <you@example.com>
2014-02-24 18:22:54 +0530

    first commit

---
+++ blog/app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+ def index
+ @post=Post.all
+ end
+ def create
+  @post=Post.new(params[:post])
+  if @post.save
+  redirect_to posts_path, :notice => "your post is saved"
+  else
+  render "new"
+  end
+  end
+
+  def new
+  @post=Post.new
+  end
+ def edit
+ @post=Post.find(params[:id])
+ end
+
+ def show
+ @post=Post.find(params[:id])
+ end
+
+ def update
+ @post=Post.find(params[:id])
+ if @post.update_attributes(params[:post])
+ redirect_to posts_path, :notice => "your post has been changes"
+ else
+ render "edit"
+ end
+ end
+
+ def destroy
+
+ end
+end
---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '9d87d530699b9881f1ec1ab4a7c0596dbf253a5226577cd6082558c18e33f6ad354f4c6a3d5be5fdb6cc1569351591e5d85b9169518bea64179a907e4b2f8188'
